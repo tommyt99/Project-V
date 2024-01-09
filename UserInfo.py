760bfe84aae7fa40ff87ad118377b61dab5939f6
Tommy Tran <tommyvtran36@gmail.com>
2024-01-08 22:31:55 -0600

    UserInfo

---
+++ UserInfo.py
@@ -0,0 +1,22 @@
+
+
+
+class UserInfo:
+    def __init__ (self, firstName, lastName, email, age=None):
+        """
+        Initialize a new user with basic information.
+
+        :param first_name: A string representing the user's first name.
+        :param last_name: A string representing the user's last name.
+        :param email: A string representing the user's email address.
+        :param age: An optional integer representing the user's age.
+        """
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.age = age
+
+
+
+
+
